dd184b2b1c4ba8129f4f0637b08973c91ca0e2b0
not oleg <104638867+fruitOleg@users.noreply.github.com>
2023-06-18 12:03:22 +0200

    upd

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,35 +1,14 @@
-import Vimeo from '@vimeo/player';
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('iframe');
-const player = new Vimeo(iframe);
-
-const onPlay = function (data) {
-  localStorage.setItem('videoplayer-current-time', JSON.stringify(data));
-};
-
-player.on('timeupdate', onPlay);
+const player = new Player(iframe);
 
 const currentTime = localStorage.getItem('videoplayer-current-time');
-console.log(currentTime);
+player.setCurrentTime(currentTime);
 
-player
-  .setCurrentTime(currentTime)
-  .then(function (seconds) {
-    // seconds = the actual time that the player seeked to
-    //  localStorage.setItem('videoplayer-current-time', JSON.parse(seconds));
-    JSON.parse(currentTime.seconds);
-  })
-  .catch(function (error) {
-    switch (error.name) {
-      case 'RangeError':
-        console.log(
-          'the time was less than 0 or greater than the video’s duration'
-        );
-        break;
+const onPlay = function (data) {
+  localStorage.setItem('videoplayer-current-time', data.seconds);
+};
 
-      default:
-        console.log('some other error occurred');
-
-        break;
-    }
-  });
+player.on('timeupdate', throttle(onPlay, 1000));
