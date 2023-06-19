9d2830a97f4e006026cd9f4c9d2ab5a44323c374
not oleg <104638867+fruitOleg@users.noreply.github.com>
2023-06-19 10:27:48 +0200

    upd

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,35 @@
 import throttle from 'lodash.throttle';
+
+const formEl = document.querySelector('.feedback-form');
+const formEmailEl = document.querySelector('.email');
+const formTextEl = document.querySelector('.message');
+
+formEl.addEventListener('submit', submitUserForm);
+formEl.addEventListener('input', throttle(inputFeedbackForm, 500));
+
+saveDataUser();
+
+function submitUserForm(event) {
+  event.preventDefault;
+
+  console.log(JSON.parce(localStorage.getItem('feedback-form-state')));
+
+  event.currentTarget.reset();
+  localStorage.removeItem('feedback-form-state');
+}
+
+function inputFeedbackForm(event) {
+  const dataForm = {};
+  dataForm[event.target.name] = event.target.value;
+
+  localStorage.setItem('feedback-form-state', JSON.stringify(dataForm));
+}
+
+function saveDataUser() {
+  const saveData = JSON.parse(localStorage.getItem('feedback-form-state'));
+
+  if (saveData) {
+    formEmailEl.value = saveData.email || '';
+    formTextEl.value = saveData.message || '';
+  }
+}
